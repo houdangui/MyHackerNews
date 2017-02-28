fc2d70f599b8f9956d210d26284ae54fc4619293
Hou Dangui <dangui@user.com.sg>
2017-02-28 15:37:02 +0800

    added Volley to send http requests

--- app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListActivity.java
+++ app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListActivity.java
@@ -1,7 +1,9 @@
-package com.hackernews.dangui.myhackernews;
+package com.hackernews.dangui.myhackernews.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.hackernews.dangui.myhackernews.R;
 
 public class NewsListActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/hackernews/dangui/myhackernews/util/HttpRequestHelper.java
@@ -0,0 +1,56 @@
+package com.hackernews.dangui.myhackernews.util;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by dangui on 28/2/17.
+ */
+
+public class HttpRequestHelper {
+    private static HttpRequestHelper ourInstance = new HttpRequestHelper();
+
+    public static HttpRequestHelper getInstance() {
+        return ourInstance;
+    }
+
+    private RequestQueue requestQueue = null;
+
+    private HttpRequestHelper() {
+
+    }
+
+    public RequestQueue getRequestQueue(Context context) {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context);
+        }
+        return requestQueue;
+    }
+
+    public void get(Context context, String url, Response.Listener<String> listener,
+                    Response.ErrorListener errorListener) {
+        sendHttpRequest(context, Request.Method.GET, url, listener, errorListener);
+    }
+
+    public void post(Context context, String url, Response.Listener<String> listener,
+                     Response.ErrorListener errorListener) {
+        sendHttpRequest(context, Request.Method.POST, url, listener, errorListener);
+    }
+
+    public void sendHttpRequest(Context context, int method, String url, Response.Listener<String> listener,
+                                Response.ErrorListener errorListener) {
+        RequestQueue queue = getRequestQueue(context);
+
+        StringRequest stringRequest = new StringRequest(method, url, listener,
+                errorListener){
+
+        };
+
+        queue.add(stringRequest);
+    }
+}
