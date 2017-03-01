0f7b7f03ec57c4cf2d389c966c8ce9e4c3613d41
Hou Dangui <dangui@user.com.sg>
2017-03-01 11:58:37 +0800

    adde  layout for news list activity

--- app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListActivity.java
+++ app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListActivity.java
@@ -1,18 +1,51 @@
 package com.hackernews.dangui.myhackernews.ui;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.hackernews.dangui.myhackernews.R;
+import com.hackernews.dangui.myhackernews.model.Story;
 import com.hackernews.dangui.myhackernews.util.HackerNewsApi;
 
-public class NewsListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class NewsListActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+    private ArrayList<Story> mTopStories;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mNewsList;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list);
 
-        HackerNewsApi.getInstance().fetchTopStories(this);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mNewsList = (RecyclerView) findViewById(R.id.news_list);
+        mLayoutManager = new LinearLayoutManager(this);
+        mNewsList.setLayoutManager(mLayoutManager);
+        mTopStories = new ArrayList<>();
+        mAdapter = new NewsListAdapter(mTopStories);
+        mNewsList.setAdapter(mAdapter);
     }
+
+    @Override
+    public void onRefresh() {
+
+    }
+
+    private void onItemsLoadComplete() {
+        // Update the adapter and notify data set changed
+
+        // Stop refresh animation
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    //HackerNewsApi.getInstance().fetchTopStories(this);
 }
---
+++ app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListAdapter.java
@@ -0,0 +1,63 @@
+package com.hackernews.dangui.myhackernews.ui;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hackernews.dangui.myhackernews.R;
+import com.hackernews.dangui.myhackernews.model.Story;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dangui on 1/3/17.
+ */
+
+public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHolder> {
+    private ArrayList<Story> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public NewsListAdapter(ArrayList<Story> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public NewsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.news_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        //holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
