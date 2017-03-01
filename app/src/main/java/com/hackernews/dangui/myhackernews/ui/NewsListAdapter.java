6d169f8aee63048e9f463bde582146777562c718
Hou Dangui <dangui@user.com.sg>
2017-03-01 15:22:01 +0800

    added  story item layout

--- app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListAdapter.java
+++ app/src/main/java/com/hackernews/dangui/myhackernews/ui/NewsListAdapter.java
@@ -17,11 +17,7 @@ import java.util.ArrayList;
 public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHolder> {
     private ArrayList<Story> mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         public TextView mTextView;
         public ViewHolder(TextView v) {
             super(v);
@@ -29,33 +25,25 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public NewsListAdapter(ArrayList<Story> myDataset) {
         mDataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public NewsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
-        // create a new view
         TextView v = (TextView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.news_item, parent, false);
-        // set the view's size, margins, paddings and layout parameters
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         //holder.mTextView.setText(mDataset[position]);
 
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mDataset.size();
