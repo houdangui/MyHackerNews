15f1c2e23fec67d3bc29d559979a411333341d72
Hou Dangui <dangui@user.com.sg>
2017-03-01 22:14:18 +0800

    added basic layout for detail page

--- app/src/main/java/com/hackernews/dangui/myhackernews/ui/ArticleFragment.java
+++ app/src/main/java/com/hackernews/dangui/myhackernews/ui/ArticleFragment.java
@@ -1,10 +1,25 @@
 package com.hackernews.dangui.myhackernews.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hackernews.dangui.myhackernews.R;
 
 /**
  * Created by dangui on 1/3/17.
  */
 
 public class ArticleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_article, container, false);
+        return view;
+    }
 }
