5aa378d599ccc39d0002e9a8f33a0a30c401ba95
Hou Dangui <dangui@user.com.sg>
2017-03-03 12:06:43 +0800

    added a simple Unit test

--- app/src/test/java/com/hackernews/dangui/myhackernews/ExampleUnitTest.java
+++ app/src/test/java/com/hackernews/dangui/myhackernews/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.hackernews.dangui.myhackernews;
 
+import com.hackernews.dangui.myhackernews.api.HackerNewsApi;
+import com.hackernews.dangui.myhackernews.util.Utils;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +16,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void LongArrayToStringVerify() {
+        String strKids = "[13779451, 13779513, 13779002, 13779836, 13778843, 13778931, 13778696]";
+        Long[] kids = new Long[]{13779451L, 13779513L, 13779002L, 13779836L, 13778843L, 13778931L, 13778696L};
+        assertEquals(strKids, Utils.longArrayToString(kids));
+
+        Long[] kidsFromStr = Utils.stringToLongArray(strKids);
+        assertArrayEquals(kids, kidsFromStr);
     }
 }
